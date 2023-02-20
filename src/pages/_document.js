e1ae5d2813f08979c02acaea315b6559760c63b7
Faisal <fasinadem108@gmail.com>
2023-02-20 11:44:37 +0500

    title added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <head>
+        <title>
+        redicalreflexp
+        </title>
+      </head>
       <body>
         <Main />
         <NextScript />
